dba5c6d87493507155bd2a13a024f52cac18cfcf
Jordan Edelston <jedelston@hp.com>
2025-02-17 19:35:39 +0000

    Init storybook.

---
+++ packages/react-forms-dev/src/stories/Field.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Meta, StoryFn} from '@storybook/react';
+
+import HelloWorld from 'react-forms-store-ux';
+
+export default {
+    title: 'Example/HelloWorld',
+    component: HelloWorld,
+} as Meta;
+
+const Template: StoryFn = args => <HelloWorld {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
